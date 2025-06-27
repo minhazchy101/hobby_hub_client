da090fecebed43a918497b7c8acc1b3fa82492c1
minhazchy101 <minhazchowdhury101@gmail.com>
2025-06-27 08:33:01 +0600

    all groups done

--- src/Pages/AllGroups.jsx
+++ src/Pages/AllGroups.jsx
@@ -8,11 +8,18 @@ const createGroups = useLoaderData()
     
     return (
        <div className="my-5 w-full max-w-7xl mx-auto">
-         <h1 className="text-2xl font-semibold mb-4">All Groups: {createGroups?.length}</h1>
-       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0.5 md:gap-3 lg:gap-6">
+        <div className="text-center my-8">
+          <h2 className="text-4xl font-bold mb-2">All Interest Groups</h2>
+<p className="text-lg italic text-gray-500">
+  "From creativity to adventure — there’s something for everyone."
+</p>
+
+        </div>
+         {/* <h1 className="text-2xl font-semibold mb-4">All Groups: {createGroups?.length}</h1> */}
+       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-0.5 md:gap-3 lg:gap-6">
        
        {
-        createGroups.map(createGroup => <div key={createGroup._id} className="card bg-base-100 shadow-sm">
+        createGroups.map(createGroup => <div key={createGroup._id} className="card bg-base-100 shadow-xl">
   <figure className="h-68 overflow-hidden">
     <img
       src={createGroup?.imageURL}  className="object-cover w-full h-full" />
@@ -20,7 +27,7 @@ const createGroups = useLoaderData()
   <div className="card-body">
     <h2 className="card-title">
       {createGroup?.groupname}
-      <div className="badge badge-secondary">{createGroup?.hobbyCategory}</div>
+    
     </h2>
     <p>{createGroup.description}</p>
       
